e640819e0b3c53226dd5b3e71188f134bb848250
root <root@Islem>
2026-04-04 15:23:04 +0100

    fix database

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const STERIFLOW_CONFIG = {
     githubUsername: 'Yassine-Gharbi86',
 
 
-    supabaseUrl:     'https://djrxmohvcloqjjgrzooe.supabase.com',
-    supabaseAnonKey: 'sb_publishable_QVK8sq_4dNJ3TkptPUA7ag_jmEhGZsW'
+    supabaseUrl:     'https://djrxmohvcloqjjgrzooe.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqcnhtb2h2Y2xvcWpqZ3J6b29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMDgwNDQsImV4cCI6MjA5MDg4NDA0NH0.eSjzVeO_q_q-KkbBVB3Sz7zDaXgKnahNP0jByAgJEQY'
 };
 
 // ─── URL helpers ───────────────────────────────────────────────────────────────
